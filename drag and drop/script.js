17bc10d27bf8b1757c5b191f106c3b6dc09cccb7
Sone <sthaya@gmail.com>
2020-03-31 19:30:58 -0700

    creatd functions for the drag and drop features, project completed

--- drag and drop/script.js
+++ drag and drop/script.js
@@ -1,5 +1,5 @@
-const draggables = document.querySelectorAll('.draggable');
-const containers = document.querySelectorAll('.container');
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
 
 draggables.forEach(draggable => {
   draggable.addEventListener('dragstart', () => {
@@ -12,18 +12,28 @@ draggables.forEach(draggable => {
 })
 
 containers.forEach(container => {
-  container.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    const afterElement = getDragAfterElement(container, e.clientY);
-    const draggable = document.querySelector('.dragging');
-    container.appendChild(draggable)
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
   })
 })
 
 function getDragAfterElement(container, y) {
-  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging')]
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
-  draggableElements.reduce((closest, child) => {
+  return draggableElements.reduce((closest, child) => {
     const box = child.getBoundingClientRect()
-  }, {offset: Number.POSITIVE_INFINITY})
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
 }
