f04f2379a34db58beb9ca75e21bb1cd8eb59c64b
Sone <sthaya@gmail.com>
2020-04-11 19:20:47 -0700

    added a filter funtion to filter completed and uncompleted list items

--- todo list/app.js
+++ todo list/app.js
@@ -2,9 +2,12 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 
 // event listeners
 todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteCheck)
+filterOption.addEventListener('click', filterTodo)
 
 //functions
 function addTodo(event) {
@@ -33,4 +36,48 @@ function addTodo(event) {
 
   //clear todo input value;
   todoInput.value = '';
+}
+
+function deleteCheck(event) {
+  const item = event.target
+  // delete todo
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    // animation
+    todo.classList.add('fall')
+    todo.addEventListener('transitionend', function () {
+      todo.remove()
+    })
+  }
+
+  // check mark
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed')
+  }
+}
+
+function filterTodo(event) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  })
 }
